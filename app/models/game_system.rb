9bc40edc3bc0d5e0054f0bebc9e3aafe29a6b3b3
tporznak <porznak@gmail.com>
2019-10-15 21:08:03 -0500

    added data to models

--- app/models/game_system.rb
+++ app/models/game_system.rb
@@ -1,5 +1,5 @@
 class GameSystem < ApplicationRecord
   has_many :characters
-  validates :name presence:true
+
 
 end
--- app/models/region.rb
+++ app/models/region.rb
@@ -2,6 +2,6 @@ class Region < ApplicationRecord
   has_many :characters
   has_one :race
   has_one :city
-  validates :name presence:true
+
 
 end
