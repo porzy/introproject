a0467dab003a0cef2ccb833a9efe5bd64219e51b
tporznak <porznak@gmail.com>
2019-10-23 17:03:30 -0500

    added all controllers

---
+++ app/controllers/character_classes_controller.rb
@@ -0,0 +1,7 @@
+class CharacterClassesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- app/models/game_system.rb
+++ app/models/game_system.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class GameSystem < ApplicationRecord
   has_many :characters
 end
