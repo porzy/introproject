7e8b2eaebbf895249d482805b12c4fd2ea4db9e6
tporznak <porznak@gmail.com>
2019-10-23 19:06:35 -0500

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  get 'regions/index'
+  get 'regions/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
