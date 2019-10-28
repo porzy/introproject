f8be14302b54e320d299d4e2aae467c25ee20527
tporznak <porznak@gmail.com>
2019-10-27 21:16:39 -0500

    edited controller for characters

--- Gemfile
+++ Gemfile
@@ -9,7 +9,7 @@ gem 'rails', '~> 6.0.0'
 gem 'sqlite3', '~> 1.4'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
-gem 'yarn', '~> 0.1.1'
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5'
 # Transpile app-like JavaScript. Read more: https://github.com/rails/webpacker
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  get '/characters', to: 'characters#index'
+  get '/characters/:id', to: 'characters#show', id: /\d+/
+  root to: 'characters#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
